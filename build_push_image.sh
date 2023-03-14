0f86c4aeb480d3a23166b0338bbfb5e6fbf5a6db
rafi <rafi.annas2000@gmail.com>
2023-03-14 22:05:56 +0700

    memperbaiki build_sh_image

--- build_push_image.sh
+++ build_push_image.sh
@@ -2,8 +2,9 @@ docker build item-app:v1 .        #menjalaskan pembuatan Docker image bernama it
 
 docker images           #menjelaskan melihat daftar image yang dimiliki
 
-docker tag item-app:v1 rafiannas/item-app     #menjelaskan perubahan nama image agar sesuai dengan format docker hub
+#karena perbaikan .. jadi syntax dimulai dari sini, tidak dari awal
+docker tag item-app rafiannas/item-app:v1     #menjelaskan perubahan nama image agar sesuai dengan format docker hub
 
 echo $PASSWORD_DOCKER_HUB | docker login -u rafiannas --password-stdin      #login ke docker hub
 
-docker push rafiannas/item-app         #menjelaskan pengunggahan image ke docker hub
+docker push rafiannas/item-app:v1         #menjelaskan pengunggahan image ke docker hub
