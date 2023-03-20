88c31024e151da31c78ed7818a7be036cf5c29f3
rafi <rafi.annas2000@gmail.com>
2023-03-20 20:57:03 +0700

    rebuild

--- build_push_image.sh
+++ build_push_image.sh
@@ -3,8 +3,8 @@ docker build item-app:v1 .        #menjalaskan pembuatan Docker image bernama it
 docker images           #menjelaskan melihat daftar image yang dimiliki
 
 #karena perbaikan .. jadi syntax dimulai dari sini, tidak dari awal
-docker tag rafiannas/item-app:v1 rafiannas/item-app:v2     #menjelaskan perubahan nama image agar sesuai dengan format docker hub
+docker tag item-app:v1 rafiannas/item-app:v1     #menjelaskan perubahan nama image agar sesuai dengan format docker hub
 
 echo $PASSWORD_DOCKER_HUB | docker login -u rafiannas --password-stdin      #login ke docker hub
 
-docker push rafiannas/item-app:v2         #menjelaskan pengunggahan image ke docker hub
+docker push rafiannas/item-app:v1         #menjelaskan pengunggahan image ke docker hub
